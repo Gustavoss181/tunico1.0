752a32ea1e2b00503b3551c07e9479e0481ef600
Gustavoss181 <69798795+Gustavoss181@users.noreply.github.com>
2023-04-18 22:28:19 -0300

    Add files via upload

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,19 +1,41 @@
-import 'package:tunico/views/pages/criar_conta/index.dart';
-import 'package:tunico/views/pages/home/index.dart';
-import 'package:tunico/views/pages/login/index.dart';
+import 'package:bicjr_app/views/pages/ajuda/index.dart';
+import 'package:bicjr_app/views/pages/cadastro/index.dart';
+import 'package:bicjr_app/views/pages/conteudos/index.dart';
+import 'package:bicjr_app/views/pages/home/index.dart';
+import 'package:bicjr_app/views/pages/login/index.dart';
+import 'package:bicjr_app/views/pages/perfil/index.dart';
+import 'package:bicjr_app/views/pages/postagens/index.dart';
+import 'package:bicjr_app/views/pages/questoes_comentadas/index.dart';
+import 'package:bicjr_app/views/pages/tela_inicial/index.dart';
+import 'package:bicjr_app/views/pages/testes.dart';
+import 'package:bicjr_app/views/pages/video_aulas/index.dart';
 
 abstract class AppRoutes{
-  static const String CONTA = 'CONTA';
-  static const String CRIAR_CONTA = 'CRIAR_CONTA';
-  static const String HOME = 'HOME';
-  static const String ITEM = 'ITEM';
-  static const String LOGIN = 'LOGIN';
+    static const String LOGIN = 'LOGIN';
+    static const String TELAINICIAL = 'TELAINICIAL';
+    static const String CADASTRO = 'CADASTRO';
+    static const String HOME = 'HOME';
+    static const String CONTEUDOS = 'CONTEUDOS';
+    static const String PERFIL = 'PERFIL';
+    static const String VIDEOAULAS = 'VIDEOAULAS';
+    static const String POSTAGENS = 'POSTAGENS';
+    static const String AJUDA = 'AJUDA';
+    static const String QUESTOESCOMENTADAS = 'QUESTOESCOMENTADAS';
+    // APAGAR
+    static const String TESTES = 'TESTES';
 
-  static final routes = {
-    //CONTA: (context) => ContaPage(),
-    CRIAR_CONTA: (context) => CriarContaPage(),
-    HOME: (context) => HomePage(),
-    //ITEM: (context) => ItemPage(),
-    LOGIN: (context) => LoginPage(),
-  };
+    static final routes = {
+      LOGIN: (context) => LoginPage(),
+      TELAINICIAL: (context) => TelaInicial(),
+      CADASTRO: (context)=> TelaCadastro(),
+      HOME: (context)=> HomePage(),
+      CONTEUDOS: (context)=> ContentsPage(),
+      PERFIL: (context)=> PerfilUsuario(),
+      VIDEOAULAS: (context)=> VideoAulas(),
+      POSTAGENS: (context)=> PostagensPage(),
+      AJUDA: (context)=> AjudaPage(),
+      QUESTOESCOMENTADAS: (context)=> QuestoesComentadasPage(),
+      // APAGAR
+      TESTES: (context)=> TestesPage()
+    };
 }
